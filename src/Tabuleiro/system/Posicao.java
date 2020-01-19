db54fb141493837181a28eac2cefb28b6c86c7d1
Rodrigo Franco <rodrigopf1905@gmail.com>
2020-01-19 15:54:18 -0300

    Posicao

---
+++ src/Tabuleiro/system/Posicao.java
@@ -0,0 +1,39 @@
+package Tabuleiro.system;
+
+public class Posicao {
+private int linha;
+private int coluna;
+
+public int getLinha() {
+	return linha;
+}
+
+public void setLinha(int linha) {
+	this.linha = linha;
+}
+
+public int getColuna() {
+	return coluna;
+}
+
+public void setColuna(int coluna) {
+	this.coluna = coluna;
+}
+
+public Posicao(int linha, int coluna) {
+	this.linha = linha;
+	this.coluna = coluna;
+}
+
+@Override
+public String toString() {
+	return linha + "/" + coluna;
+}
+
+public void SetValues(int linha , int coluna) {
+	this.linha = linha;
+	this.coluna = coluna;
+}
+
+
+}
